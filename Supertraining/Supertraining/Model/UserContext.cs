cc221e94d88a0a0dff7fdd0e7f157fa45c79cb66
Israel Deorce <israel.deorce@acad.pucrs.br>
2019-01-24 10:29:31 -0200

    adding the project to origin repo

---
+++ Supertraining/Supertraining/Model/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperTraining.Model
+{
+    public class UserContext : DbContext
+    {
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+
+        // Define the tables that we want to model
+        public DbSet<User> User { get; set; }
+    }
+}
